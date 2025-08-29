d56d2247805116cfcfbf9e31f87bbd4da9c8532c
Pekka Enberg <penberg@iki.fi>
2025-08-29 14:49:48 +0300

    Remove automatic signup

--- install.sh
+++ install.sh
@@ -186,11 +186,6 @@ main() {
   printf "\nTurso CLI installed!\n\n"
   printf "If you are a new user, you can sign up with ${bright_blue}turso auth signup${reset}.\n\n"
   printf "If you already have an account, please login with ${bright_blue}turso auth login${reset}.\n\n"
-
-  # if TURSO_INSTALL_SKIP_SIGNUP is set, then skip the sign up command
-  if [ -z "$TURSO_INSTALL_SKIP_SIGNUP" ]; then
-    $INSTALL_DIRECTORY/turso auth signup
-  fi
 }
 
 main
